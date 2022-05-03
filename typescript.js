5f2a85189de07c97fb2606385d4dd0dbb8128ae7
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2022-05-03 16:16:34 +0200

    fix issues with back-end config and add new rules

--- typescript.js
+++ typescript.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ['./back.js', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    // @see: https://github.com/typescript-eslint/typescript-eslint/issues/1824
+    '@typescript-eslint/indent': 'off',
+  },
 };
